f27636e86236db2fcafa90cdef03ecd8e9048a80
Robert Sigler <sigler@improvisedscience.org>
2019-02-18 13:50:33 +0000

    Added multiprocessing to list-repos

--- codecommit/codecommit-list-repos.py
+++ codecommit/codecommit-list-repos.py
@@ -1,14 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import boto3
-import pprint
+import concurrent.futures
+
 
 session = boto3.session.Session()
 client = session.client('codecommit')
+
+def getRepoData(repoName):
+    try:
+        repoInfo = client.get_repository(repositoryName=repoName)
+        return repoInfo["repositoryMetadata"]
+    except: 
+        raise
+
+
+
 repoList = client.list_repositories()
-
-pp = pprint.PrettyPrinter(indent=4)
-
+repos = []
 for repo in repoList["repositories"]:
-    repoName = repo["repositoryName"]
-    print(repoName)
+    repos.append(repo["repositoryName"])
+
+reposData = {}
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    future_to_repo = {executor.submit(getRepoData, repo): repo for repo in repos}
+    for future in concurrent.futures.as_completed(future_to_repo):
+        repo = future_to_repo[future]
+        try:
+            reposData[repo] = future.result()
+        except Exception as exc:
+            print("{} generated an exception: {}".format(url, exc))
+
+for repo in sorted(reposData):
+    descr = reposData[repo].get("repositoryDescription")
+    print("{0:20s} {1}".format(repo, descr))
