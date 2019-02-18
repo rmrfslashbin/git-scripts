8a548a78f500e2e80c5dede016d57bd6a512e9e5
Robert Sigler <sigler@improvisedscience.org>
2019-02-18 19:04:19 +0000

    Updated github scripts to python 3

--- github/github-delete-repo.py
+++ github/github-delete-repo.py
@@ -1,26 +1,39 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from github import Github
 from os import environ
+import argparse
 
 try:
     t = environ['GITHUB_TOKEN']
 except KeyError:
     raise SystemExit("GITHUB_TOKEN env var is not set")
+try:
+    user = environ['GITHUB_USER']
+except KeyError:
+    raise SystemExit("GITHUB_USER env var is not set")
+
 g = Github(t)
+parser = argparse.ArgumentParser(description='Delete Github repo')
+parser.add_argument("--repo", "-r", required=True, type=str, nargs="+", help="Repo name")
+parser.add_argument("--user", "-u", type=str, nargs="+", help="Github user")
+parser.add_argument("--confirm", "-c", required=True, action="store_true", help="Confirm repo deletion")
+args = parser.parse_args()
 
-repoName = raw_input("Name of repo to delete: ")
-try:
-    r = g.get_repo(repoName)
-except BaseException:
-    raise
 
-confirm = raw_input(
-    "Enter YES to Confirm you intend to delete ({}): ".format(repoName))
-if confirm != "YES":
-    raise SystemExit("User did not respond YES")
+if args.user:
+    user = args.user
 
-try:
-    r.delete()
-except BaseException:
-    raise
+for repo in args.repo:
+    repoName = "{}/{}".format(user, repo)
+    try:
+        r = g.get_repo(repoName)
+
+    except BaseException:
+        raise
+
+    try:
+        r.delete()
+        print("Deleted {}".format(repoName))
+    except BaseException:
+        raise
--- github/github-list-repos.py
+++ github/github-list-repos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from github import Github
 from os import environ
